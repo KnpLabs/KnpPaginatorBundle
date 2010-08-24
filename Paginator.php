80105438bbacfb5f2a5177ef8403f067e72fed9d
ornicar <thibault.duplessis@gmail.com>
2010-08-25 00:08:30 +0200

    Allow scrolling style autoloading

--- Paginator.php
+++ Paginator.php
@@ -16,4 +16,21 @@ class Paginator extends ZendPaginator
 
         parent::__construct($adapter);
     }
+
+    /**
+     * @see ZendPaginator::_loadScrollingStyle 
+     * Uses autoload to load the scrolling style class 
+     */
+    protected function _loadScrollingStyle($scrollingStyle = null)
+    {
+        if ($scrollingStyle === null) {
+            $scrollingStyle = self::$_defaultScrollingStyle;
+        }
+
+        if(is_string($scrollingStyle)) {
+            class_exists('Zend\\Paginator\\ScrollingStyle\\'.$scrollingStyle);
+        }
+
+        return parent::_loadScrollingStyle($scrollingStyle);
+    }
 }
