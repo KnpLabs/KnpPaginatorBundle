c9f91807395bda6165ae76cf45f1b129301f7c64
ornicar <thibault.duplessis@gmail.com>
2010-08-25 00:08:40 +0200

    Add more tests

--- Tests/PaginatorTest.php
+++ Tests/PaginatorTest.php
@@ -8,6 +8,7 @@ use Bundle\DoctrinePaginatorBundle\Paginator;
 class PaginatorTest extends BaseDatabaseTest
 {
     protected $dataCount = 45;
+    protected static $kernel;
 
     public function testDataSet()
     {
@@ -84,11 +85,119 @@ class PaginatorTest extends BaseDatabaseTest
         $query = $this->createQuery();
         $paginator = new Paginator($query);
         $pages = $paginator->getPages();
-        var_dump($pages);die;
+        $this->assertTrue(is_object($pages));
+        $this->assertEquals(1, $pages->first);
+        $this->assertEquals(5, $pages->last);
+        $this->assertEquals(1, $pages->firstPageInRange);
+        $this->assertEquals(5, $pages->lastPageInRange);
+    }
+
+    public function testRenderPaginationControlPage1()
+    {
+        $query = $this->createQuery();
+        $paginator = new Paginator($query);
+        $rendered = '<div class="paginationControl">1 - 10 of 45 <span class="disabled">First</span> | <span class="disabled">&lt; Previous</span> | <a href="2">  Next &gt; </a> | <a href="5">  Last </a></div>';
+        $this->assertEquals($rendered, $this->renderPaginationControl($paginator));
+    }
+
+    public function testRenderPaginationControlPage6()
+    {
+        $query = $this->createQuery();
+        $paginator = new Paginator($query);
+        $paginator->setPageRange(3);
+        $paginator->setCurrentPageNumber(6);
+        $paginator->setItemCountPerPage(3);
+        $rendered = '<div class="paginationControl">16 - 18 of 45 <a href="1">  First </a> | <a href="5">  &lt; Previous </a> | <a href="7">  Next &gt; </a> | <a href="15">  Last </a></div>';
+        $this->assertEquals($rendered, $this->renderPaginationControl($paginator));
+    }
+
+    protected function renderPaginationControl(Paginator $paginator)
+    {
+        ob_start();
+        $pager = $paginator->getPages();
+?> 
+<?php if ($pager->pageCount): ?>
+<div class="paginationControl">
+<?php echo $pager->firstItemNumber; ?> - <?php echo $pager->lastItemNumber; ?> of <?php echo $pager->totalItemCount; ?>
+<?php if (isset($pager->previous)): ?>
+  <a href="<?php echo $pager->first; ?>">
+    First
+  </a> |
+<?php else: ?>
+  <span class="disabled">First</span> |
+<?php endif; ?>
+<?php if (isset($pager->previous)): ?>
+  <a href="<?php echo $pager->previous; ?>">
+    &lt; Previous
+  </a> |
+<?php else: ?>
+  <span class="disabled">&lt; Previous</span> |
+<?php endif; ?>
+<?php if (isset($pager->next)): ?>
+  <a href="<?php echo $pager->next; ?>">
+    Next &gt;
+  </a> |
+<?php else: ?>
+  <span class="disabled">Next &gt;</span> |
+<?php endif; ?>
+<?php if (isset($pager->next)): ?>
+  <a href="<?php echo $pager->last; ?>">
+    Last
+  </a>
+<?php else: ?>
+  <span class="disabled">Last</span>
+<?php endif; ?>
+</div>
+<?php endif; ?>
+<?php
+        return trim(str_replace(array("  ", "\n"), array(" ", ""), ob_get_clean()));
+    }
+
+    public function testRealCasePage1()
+    {
+        $query = $this->createQuery();
+        $paginator = new Paginator($query);
+        $paginator->setPageRange(3);
+        $paginator->setCurrentPageNumber(1);
+        $paginator->setItemCountPerPage(3);
+        $pages = $paginator->getPages();
+        $this->assertTrue(is_object($pages));
+        $this->assertEquals(1, $pages->first);
+        $this->assertEquals(15, $pages->last);
+        $this->assertEquals(1, $pages->firstPageInRange);
+        $this->assertEquals(3, $pages->lastPageInRange);
+        $items = $paginator->getCurrentItems();
+        $this->assertEquals(3, count($items));
+        $this->assertEquals('test 11', reset($items)->title);
+        $this->assertEquals('test 13', end($items)->title);
+    }
+
+    public function testRealCasePage3()
+    {
+        $query = $this->createQuery();
+        $paginator = new Paginator($query);
+        $paginator->setPageRange(3);
+        $paginator->setCurrentPageNumber(3);
+        $paginator->setItemCountPerPage(3);
+        $pages = $paginator->getPages();
+        $this->assertTrue(is_object($pages));
+        $this->assertEquals(1, $pages->first);
+        $this->assertEquals(15, $pages->last);
+        $this->assertEquals(2, $pages->firstPageInRange);
+        $this->assertEquals(4, $pages->lastPageInRange);
+        $items = $paginator->getCurrentItems();
+        $this->assertEquals(3, count($items));
+        $this->assertEquals('test 17', reset($items)->title);
+        $this->assertEquals('test 19', end($items)->title);
+    }
+
+    public static function setupBeforeClass()
+    {
+        self::$kernel = self::createKernel();
     }
 
     protected function createQuery()
     {
-        return self::createKernel()->getContainer()->get('doctrine.odm.mongodb.document_manager')->getRepository('Bundle\DoctrinePaginatorBundle\Document\Test')->createQuery()->sort('title', 'asc');
+        return self::$kernel->getContainer()->get('doctrine.odm.mongodb.document_manager')->getRepository('Bundle\DoctrinePaginatorBundle\Document\Test')->createQuery()->sort('title', 'asc');
     }
 }
