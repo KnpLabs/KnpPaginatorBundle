581f06a1efc7ec93cf36542d1742e107a65e84be
ornicar <thibault.duplessis@gmail.com>
2010-08-24 15:58:06 +0200

    Add Bundle class, extension class and basic DIC configuration

---
+++ DoctrinePaginatorBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * (c) Thibault Duplessis <thibault.duplessis@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bundle\DoctrinePaginatorBundle;
+
+use Symfony\Framework\Bundle\Bundle as BaseBundle;
+
+class DoctrinePaginatorBundle extends BaseBundle
+{
+}
