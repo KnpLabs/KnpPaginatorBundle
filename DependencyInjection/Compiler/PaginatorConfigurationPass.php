fd59b58bc789fcac89109748d1a8401a1170df96
Massimiliano Arione <garakkio@gmail.com>
2017-06-06 14:57:51 +0200

    Make PaginatorConfigurationPass compatible with Symfony 3.3 and Symfony 4.0
    See https://github.com/symfony/symfony/issues/23054

--- DependencyInjection/Compiler/PaginatorConfigurationPass.php
+++ DependencyInjection/Compiler/PaginatorConfigurationPass.php
@@ -2,8 +2,9 @@
 
 namespace Knp\Bundle\PaginatorBundle\DependencyInjection\Compiler;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 
 class PaginatorConfigurationPass implements CompilerPassInterface
 {
@@ -19,30 +20,7 @@ class PaginatorConfigurationPass implements CompilerPassInterface
             return;
         }
 
-        $definition = $container->findDefinition('event_dispatcher');
-
-        foreach ($container->findTaggedServiceIds('knp_paginator.subscriber') as $id => $attributes) {
-            // We must assume that the class value has been correctly filled, even if the service is created by a factory
-            $class = $container->getDefinition($id)->getClass();
-
-            $refClass = new \ReflectionClass($class);
-            $interface = 'Symfony\Component\EventDispatcher\EventSubscriberInterface';
-            if (!$refClass->implementsInterface($interface)) {
-                throw new \InvalidArgumentException(sprintf('Service "%s" must implement interface "%s".', $id, $interface));
-            }
-
-            foreach ($class::getSubscribedEvents() as $event => $options) {
-                if (!is_array($options)) {
-                    $options = array($options, 0);
-                }
-                $definition->addMethodCall('addListenerService', array(
-                    $event,
-                    array($id, $options[0]),
-                    $options[1]
-                ));
-            }
-            // sf 2.1.x only
-            //$definition->addMethodCall('addSubscriberService', array($id, $class));
-        }
+        $pass = new RegisterListenersPass('event_dispatcher', 'knp_paginator.listener', 'knp_paginator.subscriber');
+        $pass->process($container);
     }
 }
