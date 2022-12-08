5db5d374bf7148beb387d451e53ce2222b7c8be3
Massimiliano Arione <garakkio@gmail.com>
2022-12-08 17:20:59 +0100

    listen to other exceptions

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,9 +5,11 @@ namespace Knp\Bundle\PaginatorBundle\EventListener;
 use OutOfRangeException;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use UnexpectedValueException;
+use function preg_match;
 
 /**
- * Intercept OutOfRangeException and throw http-related exceptions instead.
+ * Intercept OutOfRangeException/UnexpectedValueException and throw http-related exceptions instead.
  */
 final class ExceptionListener
 {
@@ -16,6 +18,28 @@ final class ExceptionListener
         $exception = $event->getThrowable();
         if ($exception instanceof OutOfRangeException) {
             $event->setThrowable(new NotFoundHttpException('Not Found.', $exception));
+        } elseif ($exception instanceof UnexpectedValueException && self::isInternalException($exception)) {
+            $event->setThrowable(new NotFoundHttpException('Not Found', $exception));
         }
+    }
+
+    private static function isInternalException(UnexpectedValueException $exception): bool
+    {
+        $messages = [
+            '/^Cannot filter by\: \[.+\] this field is not in allow list$/',
+            '/^Cannot sort by\: \[.+\] this field is not in allow list\.$/',
+            '/^Cannot sort with array parameter$/',
+            '/^ODM query must be a FIND type query$/',
+            '/^There is no component aliased by \[.+\] in the given Query$/',
+            '/^There is no component field \[.+\] in the given Query$/',
+            '/^There is no such field \[.+\] in the given Query component, aliased by \[.+\]$/',
+        ];
+        foreach ($messages as $regex) {
+            if (preg_match($regex, $exception->getMessage()) > 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
