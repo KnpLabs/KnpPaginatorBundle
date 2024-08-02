ae3e1137634dc4dd8a7f92ee26f1d1294d15fa62
W0rma <beck.worma@gmail.com>
2024-08-02 13:05:26 +0200

    Apply cs-fixer

--- src/DependencyInjection/KnpPaginatorExtension.php
+++ src/DependencyInjection/KnpPaginatorExtension.php
@@ -7,8 +7,8 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class KnpPaginatorExtension extends Extension
 {
